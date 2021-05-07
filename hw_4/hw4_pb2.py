726eada1842695e673c94dfe695cff3251496d63
francescazfl <francesca.zf.l@berkeley.edu>
2021-05-06 22:52:09 -0700

    hw4 pb2 fixed style

--- hw_4/hw4_pb2.py
+++ hw_4/hw4_pb2.py
@@ -4,7 +4,7 @@ import scipy.special
 import matplotlib.pyplot as plt
 
 
-class FunctionCounting(object):   
+class FunctionCounting(object):
 
     """Plot the number of homogeneously linearly separable dichotomies of N points 
     in general position in Euclidean d-space for d = 25 as a function of N. 
@@ -26,22 +26,20 @@ class FunctionCounting(object):
         """Get the Ns and ks"""
         self.N_start = N_start
         self.N_end = N_end
-        self.N_range = range(self.N_start, self.N_end+1)
-        self.Ns = np.arange(self.N_start, self.N_end+1)
+        self.N_range = range(self.N_start, self.N_end + 1)
+        self.Ns = np.arange(self.N_start, self.N_end + 1)
         self.d = d
         self.ks = np.arange(d)
 
     def get_coeff(self, s, k):
         """Get the binomial coefficients comprising (N, d) defined for
         all real s and integer k in equation 6"""
-        # print("k = {0}".format(k))
+        
         if k > 0:
             num = np.prod(s - np.arange(k))
-            # print(s - np.arange(k), num, num/np.math.factorial(k))
-            return num/np.math.factorial(k)
+            return num / np.math.factorial(k)
         elif k == 0:
-            # print(s*(s+1), s*(s+1)/np.math.factorial(k))
-            return s*(s+1)/np.math.factorial(k)
+            return s * (s + 1) / np.math.factorial(k)
         else:
             print("The current version is not considering k < 0")
 
@@ -49,7 +47,9 @@ class FunctionCounting(object):
         """Calculate the homogeneously linearly separable dichotomies 
         of N points in general position in Euclidean d-space
         in equation 5 with 2^N normalization"""
-        return 2*np.sum(np.array([self.get_coeff(N-1, k) for k in self.ks]))/(2**N)
+        return (
+            2 * np.sum(np.array([self.get_coeff(N - 1, k) for k in self.ks])) / (2 ** N)
+        )
 
     def get_Cs(self):
         """Get the array of the homogeneously linearly separable dichotomies 
@@ -59,8 +59,17 @@ class FunctionCounting(object):
 
     def default_Cs(self):
         """Calculate the binomial coeffs with scipy bino"""
-        return np.array([2*np.sum(np.array([scipy.special.binom(N-1, k) for k in np.arange(self.d)]))/2**N for N in self.N_range])
-    
+        return np.array(
+            [
+                2
+                * np.sum(
+                    np.array([scipy.special.binom(N - 1, k) for k in np.arange(self.d)])
+                )
+                / 2 ** N
+                for N in self.N_range
+            ]
+        )
+
     def plot(self, ifdefault=True):
         """Plot the number of homogeneously linearly separable dichotomies of N points
         in general position in Euclidean d-space for as a function of N"""
@@ -69,7 +78,11 @@ class FunctionCounting(object):
         else:
             Cs = self.get_Cs()
         plt.plot(self.Ns, Cs)
-        plt.title("Homogeneously linearly separable dichotomies of N points, d = {0}".format(self.d))
+        plt.title(
+            "Homogeneously linearly separable dichotomies of N points, d = {0}".format(
+                self.d
+            )
+        )
         plt.xlabel("N")
         plt.ylabel("$2^N$ normalized dichotomy number")
 
